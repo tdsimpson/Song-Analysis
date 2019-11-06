4c22a60bee65b6a34d35385a1478d6c7cddfabf9
Taylor Simpson <tdsimpson23@gmail.com>
2019-11-06 03:03:51 -0500

    Setup SCSS file structure and added minal styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = (props) => {
     return (
         <div className="header">
-            <h1>{props.title}</h1>
-            <p>{props.subtitle}</p>
+            <h1 className="header__title">{props.title}</h1>
+            <p className="header__subtitle">{props.subtitle}</p>
         </div>
     );
 }
